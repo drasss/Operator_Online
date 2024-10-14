800e650f1c4aedf2f6bde13ef77c0cc496f381e3
drasss <aurelien640@live.fr>
2024-10-14 20:41:57 +0200

    final

--- interface.py
+++ interface.py
@@ -14,7 +14,7 @@ import pandas as pd
 st.set_page_config(layout="wide")
 test, tab2, tab1 = st.tabs(["tests", "Résumé","TDL"])
 #--------------------------- GSHEET
-
+ 
 def gsheet_api_check(SCOPES):
     creds = None
     if os.path.exists('token.pickle'):
@@ -30,12 +30,12 @@ def gsheet_api_check(SCOPES):
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
     return creds
-
+ 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 SPREADSHEET_ID = '1szbNj6_bH4mA9HsBIJD-qKyrNFYwYyEqG-ekmoGnE5A'
-
+ 
 creds=gsheet_api_check(SCOPES)
-
+ 
 from googleapiclient.discovery import build
 def pull_sheet_data(SCOPES,SPREADSHEET_ID,DATA_TO_PULL):
     creds = gsheet_api_check(SCOPES)
@@ -43,21 +43,17 @@ def pull_sheet_data(SCOPES,SPREADSHEET_ID,DATA_TO_PULL):
     sheet = service.spreadsheets()
     result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,range=ranging).execute()
     values = result.get('values', [])
-    
     if not values:
-        print('No data found.')
+        pass
     else:
-        rows = sheet.values().get(spreadsheetId=SPREADSHEET_ID,range=ranging).execute()
-        data = rows.get('values')
-        print("COMPLETE: Data copied")
+        data = values
         return data
-
-
+ 
+ 
 #save
-
+ 
 def saving():
     service = build("sheets", "v4", credentials=creds)
-
     values = [[""]*6]*10
     body = {"values": values}
     result = (
@@ -65,12 +61,13 @@ def saving():
         .values()
         .update(
             spreadsheetId="1szbNj6_bH4mA9HsBIJD-qKyrNFYwYyEqG-ekmoGnE5A",
-            range="A1:F100",
+            range="A1:F20",
             valueInputOption="USER_ENTERED",
             body=body,
         )
         .execute())
-    
+ 
+ 
     values = [[str(TABB[i][j]) for j in range(len(TABB[i]))] for i in range(len(TABB))]
     body = {"values": values}
     result = (
@@ -78,16 +75,16 @@ def saving():
         .values()
         .update(
             spreadsheetId="1szbNj6_bH4mA9HsBIJD-qKyrNFYwYyEqG-ekmoGnE5A",
-            range="A1:F100",
+            range="A1:F20",
             valueInputOption="USER_ENTERED",
             body=body,
         )
         .execute())
-
-
+ 
+ 
     
-
-
+ 
+ 
     
 #test
 def dele():
@@ -110,40 +107,24 @@ test.file_uploader("Upload a CSV")
 test.camera_input("Take a picture")
 test.color_picker("Pick a color")
 x=test.text("rr")
-
+ 
 # tab1 TDL ----------------------------------------- A CHANGER
 ranging = 'A1:F100'
 data = pull_sheet_data(SCOPES,SPREADSHEET_ID,ranging)
 df = pd.DataFrame(data)
-
-###contenu tab
-##try : TDL==""   #------------------------
-##except :
-##    fichier=open("TDL.txt")
-##    TDL_text=fichier.read() #------------------------
-##    fichier.close()
-
-
-#transformation 
-##
-##TDL_text=TDL_text.strip("\n").split("\n") #------------------------ 
-##TDL=[TDL_text[i].split(" | ") for i in range(len(TDL_text))] #------------------------
-### numéro de ligne
-
-
-
+ 
+ 
 if 'counter' not in st.session_state:
     st.session_state['counter']=len(df) #------------------------
 button_col1,button_col2=tab1.columns(2)
-
-
-
+ 
+ 
 #------------------------------------------------------------------------------boutons add row
 tm=time.localtime()
-
+ 
 def add_ro():
     service = build("sheets", "v4", credentials=creds)
-
+ 
     values = [[False,"","--","",str(tm[0])+"-"+str(tm[1])+"-"+str(tm[2]),str(tm[3])+":"+str(tm[4])]]
     body = {"values": values}
     result = (
@@ -157,57 +138,44 @@ def add_ro():
         )
         .execute()
     )
-    
+ 
     st.session_state['counter']+=1
-
+ 
 button_col1.button("add row",on_click=add_ro)
-
+ 
 #boutons delete row
 def dlrow(i,**parameters):
     global nb,df
-    print("-----------------",i,"/",nb)
-    print(TABB[i])
-    print(df)
-    print(df.drop([i]))
+    start_time=time.time()
     df=df.drop([i])
     del TABB[i]
-
+ 
     nb-=1
     st.session_state['counter']-=1
-    print("-----")
-    print(TABB)
-    saving()
-    
-
-
+    end_time=time.time()
+ 
 nb=st.session_state['counter']
-
-
+ 
+ 
 TABB=[]
 col_tab1=[]
 for i in range(nb):
     col_tab1+=[tab1.columns([1,3,3,4,2,2],vertical_alignment="center")]
     # delete , name      ,   state    , description , 
     #button , text_input ,  selectbox , text_area , date_input , time_input
-
+ 
     TABB+=[[col_tab1[i][0].button("X",key=str(i)+"tab1button",on_click=dlrow,kwargs={"i":i}),
         col_tab1[i][1].text_input("name",key=str(i)+"tab1name",value=df[1][i]),
          col_tab1[i][2].selectbox("state",["--","Fini","En cours","A commencer","Pas de mon ressort"],key=str(i)+"tab1state",index=["--","Fini","En cours","A commencer","Pas de mon ressort"].index(df[2][i])),
          col_tab1[i][3].text_area("desc",key=str(i)+"tab1desc",value=str(df[3][i])).replace("\n"," "),
          col_tab1[i][4].date_input("Date",key=str(i)+"tab1date",value=datetime.datetime(int(df[4][i].split("-")[0]),int(df[4][i].split("-")[1]),int(df[4][i].split("-")[2]))),
         col_tab1[i][5].time_input("Time",key=str(i)+"tab1time",value=datetime.time(int(df[5][i].split(":")[0]),int(df[5][i].split(":")[1])))]]
-
-##button=[TABB[i][0] for i in range(len(TABB))]
-##print(button)
-##if sum(button)>0:
-##    dlrow(button.index(1))
-
-
+ 
+ 
 saving()
-
-
+ 
+ 
 # tab2
 RES=[]
 for i in range(nb):
     tab2.checkbox(df[1][i],key=str(i)+"resume")
-
