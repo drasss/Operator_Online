c7398e79b027576c0bcce9ac2b6bc2a8e990d07c
drasss <aurelien640@live.fr>
2024-10-19 16:04:08 +0200

    go

--- interface.py
+++ interface.py
@@ -32,7 +32,11 @@ def gsheet_api_check(SCOPES):
     return creds
  
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+<<<<<<< Updated upstream
 SPREADSHEET_ID = '1szbNj6_bH4mA9HsBIJD-qKyrNFYwYyEqG-ekmoGnE5A'
+=======
+SPREADSHEET_ID = '1A-WhcCyeP82OnuXER_JlSdWoO-EFnbnOQe2KinYo2bQ'
+>>>>>>> Stashed changes
  
 creds=gsheet_api_check(SCOPES)
  
@@ -60,7 +64,11 @@ def saving():
         service.spreadsheets()
         .values()
         .update(
+<<<<<<< Updated upstream
             spreadsheetId="1szbNj6_bH4mA9HsBIJD-qKyrNFYwYyEqG-ekmoGnE5A",
+=======
+            spreadsheetId="1A-WhcCyeP82OnuXER_JlSdWoO-EFnbnOQe2KinYo2bQ",
+>>>>>>> Stashed changes
             range="A1:F20",
             valueInputOption="USER_ENTERED",
             body=body,
@@ -74,7 +82,11 @@ def saving():
         service.spreadsheets()
         .values()
         .update(
+<<<<<<< Updated upstream
             spreadsheetId="1szbNj6_bH4mA9HsBIJD-qKyrNFYwYyEqG-ekmoGnE5A",
+=======
+            spreadsheetId="1A-WhcCyeP82OnuXER_JlSdWoO-EFnbnOQe2KinYo2bQ",
+>>>>>>> Stashed changes
             range="A1:F20",
             valueInputOption="USER_ENTERED",
             body=body,
@@ -131,7 +143,11 @@ def add_ro():
         service.spreadsheets()
         .values()
         .append(
+<<<<<<< Updated upstream
             spreadsheetId="1szbNj6_bH4mA9HsBIJD-qKyrNFYwYyEqG-ekmoGnE5A",
+=======
+            spreadsheetId="1A-WhcCyeP82OnuXER_JlSdWoO-EFnbnOQe2KinYo2bQ",
+>>>>>>> Stashed changes
             range="A1:A1",
             valueInputOption="USER_ENTERED",
             body=body,
